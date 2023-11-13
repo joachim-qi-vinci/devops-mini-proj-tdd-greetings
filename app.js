0962c0837c6571c9b1f0619be96a5d8b59d3c9fb
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-11-13 21:41:15 +0100

    perf: simplify greetWithArrayOf2() to greetWithArray()

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ function greet (name) {
   if (name === undefined || name === null || name === '') {
     return greetWithNoName()
   }
-  if (Array.isArray(name) && name.length <= 2) {
-    return greetWithArrayOf2(name)
-  }
-  if (Array.isArray(name) && name.length > 2) {
+  if (Array.isArray(name)) {
     return greetWithArray(name)
   }
   if (name === name.toUpperCase()) {
@@ -20,10 +17,6 @@ function greetWithNoName () {
 
 function greetWithUpperCaseName (name) {
   return `HELLO, ${name}!`
-}
-
-function greetWithArrayOf2 (name) {
-  return 'Hello, ' + name.join(' and ') + '.'
 }
 
 function greetWithArray (name) {
