eb31d0033df83020c9bd9f9a2bd4bcea502d35ec
margjini <mario.margjini@student.vinci.be>
2023-11-13 20:17:43 +0100

    refactor : greetWithNoName

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,14 +4,14 @@ test('should return Hello, [name].', () => {
   expect(greet('Bob')).toBe('Hello, Bob.')
 })
 
-test('should return "Hello, my friend" when name is undefined', () => {
-  expect(greet()).toBe('Hello, my friend')
+test('should return "Hello, my friend." when name is undefined', () => {
+  expect(greet()).toBe('Hello, my friend.')
 })
 
-test('should return "Hello, my friend" when name is null', () => {
-  expect(greet(null)).toBe('Hello, my friend')
+test('should return "Hello, my friend." when name is null', () => {
+  expect(greet(null)).toBe('Hello, my friend.')
 })
 
-test('should return "Hello, my friend" when name is an empty string', () => {
-  expect(greet('')).toBe('Hello, my friend')
+test('should return "Hello, my friend." when name is an empty string', () => {
+  expect(greet('')).toBe('Hello, my friend.')
 })
--- app.js
+++ app.js
@@ -2,11 +2,12 @@ function greet (name) {
   if (name === undefined || name === null || name === '') {
     return greetWithNoName()
   }
+
   return `Hello, ${name}.`
 }
 
 function greetWithNoName () {
-  return 'Hello, my friend'
+  return 'Hello, my friend.'
 }
 
 module.exports = greet
